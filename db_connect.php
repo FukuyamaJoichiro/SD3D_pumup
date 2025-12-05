c36bfa2144e0c536b4152325d2ff84d5b92e6eff
aclla12 <2301163@s.asojuku.ac.jp>
2025-12-05 12:33:33 +0900

    変更点

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,16 @@
 <?php
-/**
- * 環境によって自動で接続先 DB を切り替える
- */
-
 $hostName = $_SERVER['HTTP_HOST'] ?? '';
 
-if ($hostName === 'localhost' || $hostName === '127.0.0.1') {
-    // ローカル環境（XAMPP）
+
+
+$isLocal =
+    stripos(PHP_OS, 'WIN') === 0 ||
+    $hostName === 'localhost' ||
+    $hostName === '127.0.0.1';
+
+if ($isLocal) {
     require __DIR__ . '/config/db_connect_local.php';
 } else {
-    // 本番環境（ロリポップ）
     require __DIR__ . '/config/db_connect_prod.php';
 }
+?>
